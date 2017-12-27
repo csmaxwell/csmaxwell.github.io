59c58a4befa231ac0432bb5650be882b283df1f7
Colin Maxwell <cs.maxwell@gmail.com>
2017-12-26 19:04:31 -0500

    Nikola auto commit.
    Nikola version: 7.8.10

--- conf.py
+++ conf.py
@@ -1372,4 +1372,4 @@ COMPILERS["orgmode"] = ('.org',)
 
 # Add org files to your POSTS, PAGES
 POSTS = POSTS + (("posts/*.org", "posts", "post.tmpl"),)
-PAGES = PAGES + (("stories/*.org", "stories", "story.tmpl"),)
+PAGES = PAGES + (("stories/*.rst", "stories", "story.tmpl"),)
